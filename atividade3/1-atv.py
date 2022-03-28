d8500c5cb437e264d39c2781f51ce1ca23cd4117
Keylane <keylanessilva07@gmail.com>
2022-03-28 20:27:00 -0300

    atividade proposta pec

---
+++ atividade3/1-atv.py
@@ -0,0 +1,4 @@
+nascimento = int(input("Em que ano voce nasceu\n"))
+ano = int(input("Para que ano voce que saber a sua idade?\n"))
+print('No ano de',ano,'voce terá', ano-nascimento)
+
---
+++ atividade3/2-atv.py
@@ -0,0 +1,2 @@
+anos = int(input("Quantos ano voce tem?\n"))
+print("Se voce fosse um cachorro, voce teria", anos*7)
